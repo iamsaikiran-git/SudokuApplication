6555606cdb4ece8ee8c87355754557f418c03c25
Saikiran Chinta <Saikiran.Chinta@westpharma.com>
2023-07-02 14:24:10 +0530

    Add project files.

---
+++ SudokuApplication/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SudokuApplication.Models;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace SudokuApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int[][] board = new int[][] { new int [] {5,3,0,0,7,0,0,0,0},
+new int[] { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+new int[] { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+new int[] { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+new int[] { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+new int[] { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+new int[] { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+new int[] { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+new int[] { 0, 0, 0, 0, 8, 0, 0, 7, 9 }};
+
+            return View(board);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SudokuApplication/wwwroot/js/site.js
@@ -0,0 +1,155 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+let currentSudokuBoard; // Define a global variable
+
+let initialBoard;
+
+function initializeModel(data)
+{
+    currentSudokuBoard = data; // Assign the serialized model to the global variable
+    initialBoard = copyArray(data);
+}
+
+function onTextChanged(id, value)
+{
+    let indexArr = String(id).split(",")
+    let row = Number(indexArr[0])
+    let column = Number(indexArr[1])
+    let updatedValue = Number(value)
+    if (value != "" && updatedValue >= 1 && updatedValue <= 9) {
+        if (!IsValidSudokuPlaceMent(row, column, Number(value), "red")) {
+            document.getElementById(id).style.borderColor = "red"
+        }
+        currentSudokuBoard[row][column] = Number(value);
+    }
+    else if (updatedValue == "") {
+        IsValidSudokuPlaceMent(row, column, currentSudokuBoard[row][column], "black")
+        document.getElementById(id).style.borderColor = "black"
+        currentSudokuBoard[row][column] = 0;
+    }
+    else
+    {
+        document.getElementById(id).value = ""
+    }
+}
+
+function IsValidSudokuPlaceMent(row, column, newchar,colorToUpdate)
+{
+    for (let i = 0; i < 9; i++)
+    {
+        if (i!= column && currentSudokuBoard[row][i] != 0 && currentSudokuBoard[row][i] == newchar)
+        {
+            let id = String(row) + "," + i
+            let box = document.getElementById(String(id));
+            box.style.borderColor = colorToUpdate;
+            return false;
+        }
+        if ( i != row && currentSudokuBoard[i][column] != 0 && currentSudokuBoard[i][column] == newchar)
+        {
+            let id = String(i) + "," + column
+            document.getElementById(id).style.borderColor = colorToUpdate;
+            return false;
+        }
+        let r1 = (3 * (Math.floor(row / 3))) + (Math.floor(i / 3))
+        let c1 = (3 * (Math.floor(column / 3))) + (i % 3)
+        let boxindex = currentSudokuBoard[r1][c1]
+        if ((r1 != row && c1 != column) && boxindex != 0 && boxindex == newchar)
+        {
+            let id = String(r1) + "," + c1
+            document.getElementById(id).style.borderColor = colorToUpdate;
+            return false;
+        }
+    }
+    return true;
+}
+
+function solveSudoku()
+{
+    currentSudokuBoard = copyArray(initialBoard)
+    Sudokusolver(0, 0)
+    populateTable(currentSudokuBoard)
+}
+
+function resetSucokuBoard()
+{
+    currentSudokuBoard = copyArray(initialBoard)
+    populateTable(currentSudokuBoard)
+}
+
+function Sudokusolver(row,column)
+{
+    if (row == currentSudokuBoard.length)
+    {
+        return true;
+    }
+    if (column == currentSudokuBoard.length)
+    {
+        return Sudokusolver(row + 1, 0);
+    }
+
+    if (currentSudokuBoard[row][column] != 0)
+    {
+        return Sudokusolver(row, column + 1);
+    }
+
+    for (let k = 1; k <= 9; k++)
+    {
+        if (ValidateSudokuPlacement(row, column, k))
+        {
+            currentSudokuBoard[row][column] = k;
+            if (Sudokusolver(row, column + 1))
+            {
+                return true;
+            }
+            currentSudokuBoard[row][column] = 0;
+        }
+    }
+    return false;
+}
+
+function ValidateSudokuPlacement(row, column, newchar)
+{
+    for (let i = 0; i < 9 ; i++)
+    {
+        if (currentSudokuBoard[row][i] == newchar)
+            return false;
+        if (currentSudokuBoard[i][column] == newchar)
+            return false;
+        if (currentSudokuBoard[3 * (Math.floor(row / 3)) + (Math.floor(i / 3))][3 * (Math.floor(column / 3)) + i % 3] == newchar)
+            return false;
+    }
+    return true;
+}
+
+function copyArray(oldArray) {
+    let newArray = []
+    for (let i = 0; i < 9; i++) {
+        let newAray1 = []
+        for (let j = 0; j < 9; j++) {
+            newAray1.push(oldArray[i][j])
+        }
+        newArray.push(newAray1)
+    }
+    return newArray
+}
+
+
+function populateTable(boardToPopulate)
+{
+    for (let i = 0; i < 9; i++)
+    {
+        for (let j = 0; j < 9; j++)
+        {
+            let valueToPopulate = ""
+            if (boardToPopulate[i][j] > 0)
+            {
+                valueToPopulate = boardToPopulate[i][j]
+            }
+            document.getElementById(String(i) + "," + j).value = valueToPopulate;
+            document.getElementById(String(i) + "," + j).style.borderColor = "black"
+        }
+    }
+}
