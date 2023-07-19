8d2f3444fa67a22ab7d08a20c2a068b508fbf2a5
Saikiran Chinta <Saikiran.Chinta@westpharma.com>
2023-07-20 00:39:06 +0530

    Made logical, functional,UI improvements

--- SudokuApplication/Controllers/HomeController.cs
+++ SudokuApplication/Controllers/HomeController.cs
@@ -16,19 +16,95 @@ namespace SudokuApplication.Controllers
 
         public IActionResult Index()
         {
-            int[][] board = new int[][] { new int [] {5,3,0,0,7,0,0,0,0},
-new int[] { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
-new int[] { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
-new int[] { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
-new int[] { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
-new int[] { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
-new int[] { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
-new int[] { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
-new int[] { 0, 0, 0, 0, 8, 0, 0, 7, 9 }};
+            int[][] board = new int[][]
+            { 
+                new int[] {5,3,0,0,7,0,0,0,0},
+                new int[] {6,0,0,1,9,5,0,0,0},
+                new int[] {0,9,8,0,0,0,0,6,0},
+                new int[] {8,0,0,0,6,0,0,0,3},
+                new int[] {4,0,0,8,0,3,0,0,1},
+                new int[] {7,0,0,0,2,0,0,0,6},
+                new int[] {0,6,0,0,0,0,2,8,0},
+                new int[] {0,0,0,4,1,9,0,0,5},
+                new int[] {0,0,0,0,8,0,0,7,9}};
+            int[][] board2 = new int[][]
+            {
+                new int [] { 0,0,8,0,2,0,0,0,0},
+                new int [] { 0,0,0,0,5,7,0,2,9},
+                new int [] { 0,7,0,0,0,4,3,0,6},
+                new int [] { 0,0,5,9,6,0,0,0,0},
+                new int [] { 2,9,0,4,7,0,6,5,1},
+                new int [] { 7,8,6,3,1,0,0,4,2},
+                new int [] { 0,0,0,0,0,6,0,3,0},
+                new int [] { 3,2,0,0,4,0,0,0,0},
+                new int [] { 8,0,4,0,0,0,2,0,5}
+            };
 
-            return View(board);
+            int[][] board3 = new int[][]
+            {
+                new int [] {8,3,0,0,0,0,1,0,0},
+                new int [] {0,0,0,0,0,2,3,0,0},
+                new int [] {1,0,0,0,5,0,0,0,4 },
+                new int [] {9,8,0,1,0,5,0,7,2 },
+                new int [] {2,5,7,9,0,0,0,3,1 },
+                new int [] {6,1,3,7,2,8,0,4,0 },
+                new int [] {4,2,0,5,0,1,0,0,3 },
+                new int [] {0,7,8,0,0,9,0,0,5 },
+                new int [] {0,6,0,4,0,0,0,0,0 }
+            };
+
+            int[][] board4 = new int[][]
+            {
+                new int [] {0,7,0,0,5,0,1,0,2 },
+                new int [] {1,6,0,0,0,3,0,0,0 },
+                new int [] {5,0,0,2,9,0,0,6,4 },
+                new int [] {2,0,4,1,0,0,6,0,0 },
+                new int [] {0,0,7,3,0,0,0,1,5 },
+                new int [] {0,0,3,0,0,0,4,0,0 },
+                new int [] {7,0,0,8,0,0,3,0,0 },
+                new int [] {0,0,0,0,6,0,0,7,0 },
+                new int [] {0,0,0,4,0,7,0,5,0 }
+            };
+
+            int[][] boards5 = new int[][]
+            {
+                new int [] {0,5,0,4,9,0,6,0,7 },
+                new int [] {1,0,0,0,2,6,0,9,0 },
+                new int [] {0,9,0,0,0,8,0,0,0 },
+                new int [] {0,0,0,1,5,9,3,2,6 },
+                new int [] {0,0,0,0,0,0,0,0,9 },
+                new int [] {9,0,5,0,0,0,0,0,1 },
+                new int [] {0,0,0,0,8,7,0,6,3 },
+                new int [] {2,7,0,6,0,3,0,0,0 },
+                new int [] {0,6,0,0,0,0,7,1,0 }
+            };
+
+            int[][] boards6 = new int[][]
+            {
+                new int [] {0,1,0,0,0,0,0,4,3 },
+                new int [] {0,0,0,8,0,1,0,0,7 },
+                new int [] {5,0,0,0,0,3,0,0,8 },
+                new int [] {0,7,0,0,0,0,0,1,0 },
+                new int [] {0,5,0,0,4,0,0,8,6 },
+                new int [] {8,0,6,1,0,7,4,0,2 },
+                new int [] {0,0,1,0,0,5,9,0,0 },
+                new int [] {9,0,0,0,0,0,2,6,0 },
+                new int [] {0,4,7,2,0,0,0,0,0 }
+            };
+
+            List<int[][]> boards = new List<int[][]>();
+            boards.Add(board);
+            boards.Add(board2);
+            boards.Add(board3);
+            boards.Add(board4);
+            boards.Add(boards5);
+            boards.Add(boards6);
+            return View(boards);
         }
-
+        public IActionResult Index1()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
--- SudokuApplication/wwwroot/js/site.js
+++ SudokuApplication/wwwroot/js/site.js
@@ -3,105 +3,137 @@
 
 // Write your JavaScript code.
 
-let currentSudokuBoard; // Define a global variable
+let currentSudokuBoard;
 
 let initialBoard;
+let givenSudokus;
+let vacantCellsCount = 0;
 
-function initializeModel(data)
-{
-    currentSudokuBoard = data; // Assign the serialized model to the global variable
-    initialBoard = copyArray(data);
+//Initializes the sudoku application data
+function initializeModel(data) {
+    givenSudokus = data
+    initializeSudokuBoard(0)
 }
 
-function onTextChanged(id, value)
-{
-    let indexArr = String(id).split(",")
-    let row = Number(indexArr[0])
-    let column = Number(indexArr[1])
+//resets the sudoku board
+function resetSudokuBoard() {
+    currentSudokuBoard = copyArray(initialBoard)
+    populateTable(currentSudokuBoard)
+}
+
+// Initializes the Sudoku Board
+function initializeSudokuBoard(selectedBoardIndex) {
+    vacantCellsCount = 0
+    let tempSelectedSudoku = givenSudokus[selectedBoardIndex]
+    currentSudokuBoard = copyArray(tempSelectedSudoku)
+    initialBoard = tempSelectedSudoku
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            let cell = document.getElementById(i + ',' + j)
+            let currentCell = cell.cloneNode(true);
+            cell.parentNode.replaceChild(currentCell, cell);
+            if (tempSelectedSudoku[i][j] == 0) {
+                currentCell.disabled = false
+                currentCell.value = ""
+                currentCell.addEventListener("input", function () {
+                    onTextChanged(currentCell.id, currentCell.value)
+                })
+                vacantCellsCount += 1
+            }
+            else {
+                currentCell.value = tempSelectedSudoku[i][j]
+                currentCell.disabled = true
+            }
+        }
+    }
+}
+
+// Handles the on text changes event for each cell and upadtes the UI accordingly
+function onTextChanged(id, value) {
+    let tableCell = String(id).split(",")
+    let row = Number(tableCell[0])
+    let column = Number(tableCell[1])
     let updatedValue = Number(value)
     if (value != "" && updatedValue >= 1 && updatedValue <= 9) {
         if (!IsValidSudokuPlaceMent(row, column, Number(value), "red")) {
             document.getElementById(id).style.borderColor = "red"
         }
+        else {
+            vacantCellsCount -= 1
+        }
         currentSudokuBoard[row][column] = Number(value);
+        if (vacantCellsCount == 0) {
+            alert("Congratulations You've solved this sudoku puzzle \n check out other puzzles!!!");
+        }
     }
-    else if (updatedValue == "") {
+    else {
         IsValidSudokuPlaceMent(row, column, currentSudokuBoard[row][column], "black")
         document.getElementById(id).style.borderColor = "black"
+        if (currentSudokuBoard[row][column] > 0 && currentSudokuBoard[row][column] < 10) {
+            vacantCellsCount += 1
+        }
         currentSudokuBoard[row][column] = 0;
-    }
-    else
-    {
         document.getElementById(id).value = ""
     }
 }
 
-function IsValidSudokuPlaceMent(row, column, newchar,colorToUpdate)
-{
-    for (let i = 0; i < 9; i++)
-    {
-        if (i!= column && currentSudokuBoard[row][i] != 0 && currentSudokuBoard[row][i] == newchar)
-        {
+// Checks if the current valus user entered is correct and updates the UI Accordingly
+function IsValidSudokuPlaceMent(row, column, newchar, colorToUpdate) {
+    let result = true;
+    for (let i = 0; i < 9; i++) {
+
+        // Check horizontal row
+        if (i != column && currentSudokuBoard[row][i] != 0 && currentSudokuBoard[row][i] == newchar) {
             let id = String(row) + "," + i
-            let box = document.getElementById(String(id));
-            box.style.borderColor = colorToUpdate;
-            return false;
+            let box = document.getElementById(String(id))
+            box.style.borderColor = colorToUpdate
+            result = false;
         }
-        if ( i != row && currentSudokuBoard[i][column] != 0 && currentSudokuBoard[i][column] == newchar)
-        {
+
+        //Checks vertical column
+        if (i != row && currentSudokuBoard[i][column] != 0 && currentSudokuBoard[i][column] == newchar) {
             let id = String(i) + "," + column
             document.getElementById(id).style.borderColor = colorToUpdate;
-            return false;
+            result = false;
         }
+
+        //Checks the mini matrix
         let r1 = (3 * (Math.floor(row / 3))) + (Math.floor(i / 3))
         let c1 = (3 * (Math.floor(column / 3))) + (i % 3)
         let boxindex = currentSudokuBoard[r1][c1]
-        if ((r1 != row && c1 != column) && boxindex != 0 && boxindex == newchar)
-        {
+        if ((r1 != row && c1 != column) && boxindex != 0 && boxindex == newchar) {
             let id = String(r1) + "," + c1
             document.getElementById(id).style.borderColor = colorToUpdate;
-            return false;
+            result = false;
         }
     }
-    return true;
+    return result;
 }
 
-function solveSudoku()
-{
+//Invokes the solve sudoku action
+function solveSudoku() {
     currentSudokuBoard = copyArray(initialBoard)
     Sudokusolver(0, 0)
     populateTable(currentSudokuBoard)
 }
 
-function resetSucokuBoard()
-{
-    currentSudokuBoard = copyArray(initialBoard)
-    populateTable(currentSudokuBoard)
-}
-
-function Sudokusolver(row,column)
-{
-    if (row == currentSudokuBoard.length)
-    {
+// Checks the core logic to solve the sudoku puzzle
+function Sudokusolver(row, column) {
+    if (row == currentSudokuBoard.length) {
         return true;
     }
-    if (column == currentSudokuBoard.length)
-    {
+    if (column == currentSudokuBoard.length) {
         return Sudokusolver(row + 1, 0);
     }
 
-    if (currentSudokuBoard[row][column] != 0)
-    {
+    if (currentSudokuBoard[row][column] != 0) {
         return Sudokusolver(row, column + 1);
     }
 
-    for (let k = 1; k <= 9; k++)
-    {
-        if (ValidateSudokuPlacement(row, column, k))
-        {
+    for (let k = 1; k <= 9; k++) {
+        if (ValidateSudokuPlacement(row, column, k)) {
             currentSudokuBoard[row][column] = k;
-            if (Sudokusolver(row, column + 1))
-            {
+            if (Sudokusolver(row, column + 1)) {
                 return true;
             }
             currentSudokuBoard[row][column] = 0;
@@ -110,10 +142,9 @@ function Sudokusolver(row,column)
     return false;
 }
 
-function ValidateSudokuPlacement(row, column, newchar)
-{
-    for (let i = 0; i < 9 ; i++)
-    {
+// Validates the current sudoku placement for the row and column given
+function ValidateSudokuPlacement(row, column, newchar) {
+    for (let i = 0; i < 9; i++) {
         if (currentSudokuBoard[row][i] == newchar)
             return false;
         if (currentSudokuBoard[i][column] == newchar)
@@ -124,6 +155,7 @@ function ValidateSudokuPlacement(row, column, newchar)
     return true;
 }
 
+// creates the new copy of array
 function copyArray(oldArray) {
     let newArray = []
     for (let i = 0; i < 9; i++) {
@@ -136,17 +168,17 @@ function copyArray(oldArray) {
     return newArray
 }
 
-
-function populateTable(boardToPopulate)
-{
-    for (let i = 0; i < 9; i++)
-    {
-        for (let j = 0; j < 9; j++)
-        {
+// Populates the table with the given board object
+function populateTable(boardToPopulate) {
+    vacantCellsCount = 0
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
             let valueToPopulate = ""
-            if (boardToPopulate[i][j] > 0)
-            {
+            if (boardToPopulate[i][j] > 0) {
                 valueToPopulate = boardToPopulate[i][j]
+            }
+            else {
+                vacantCellsCount += 1
             }
             document.getElementById(String(i) + "," + j).value = valueToPopulate;
             document.getElementById(String(i) + "," + j).style.borderColor = "black"
